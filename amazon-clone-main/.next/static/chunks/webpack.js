7add481560353adfd43df260a29b1951817c95fe
lokeshdhake <dhakelokesh31@gmail.com>
2024-02-18 17:28:28 +0530

    added url for vercel deployment link

--- amazon-clone-main/.next/static/chunks/webpack.js
+++ amazon-clone-main/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f341d22566bfbb07"; }
+/******/ 		__webpack_require__.h = function() { return "ef5f31e8cc9a2208"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
